3d459aab52b3d4eef247c7f15e308fa114878723
Haneen Mahdin <haneenmahdin@gmail.com>
2021-07-07 14:37:17 +0530

    `Feature💡`: catch error if the path provided is not found

--- index.ts
+++ index.ts
@@ -6,11 +6,18 @@ class ResolveFile {
       *
       * @param {string} pathTo Path in which the program should search for the file
       * @param {string} file File to find
+      * @param {"forward" | "backward"} direction Type of search. 
+      * If backward the search will start from the existing path and goes until the root path. 
+      * If forward the search will start from the exisitng path and goes until the it does not find the file
       * @return {string} The exact path to the file
     */
-    static search(pathTo: string, file: string): string {
-        const result = "";
+    static search(pathTo: string, file: string, direction?: "forward" | "backward"): string {
         // Reducing call stack count
+        if (!fs.existsSync(path.resolve(pathTo))) {
+            console.log(`Path not found: ${path.resolve(pathTo)}`)
+            // Returning NULL if the path is not found!
+            return "NULL";
+        }
         pathTo = path.resolve(pathTo);
 
         const startPath = path.join(pathTo, file);
@@ -25,12 +32,10 @@ class ResolveFile {
 
             this.search(_nextPath, file);
         } else {
-            console.log("Here is the path:", startPath);
-
-            return result.concat(startPath);
+            return startPath;
         }
 
-        return result;
+        return startPath;
     }
 };
 
--- test.ts
+++ test.ts
@@ -1,4 +1,3 @@
 import ResolveFile from ".";
 
-console.log("Search results");
-ResolveFile.search('node_modules', 'package.json');
+console.log(ResolveFile.search('aaS', 'package.json'));
